9dd8ad1b7b9ce6326e4e703a36b431fb558e312a
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-09-24 14:48:26 +0200

    some cleanup

--- Scripts/ex2_1_2.m
+++ Scripts/ex2_1_2.m
@@ -1,9 +1,13 @@
 %% exercise 2.1.2
 cdir = fileparts(mfilename('fullpath')); 
-[A, D] = tmg(fullfile(cdir,'../Data/textDocs.txt'));
+OPTIONS.stoplist = fullfile(cdir,'../Data/stopWords.txt');
+OPTIONS.stemming = 1;
+[A, D] = tmg(fullfile(cdir,'../Data/textDocs.txt'), OPTIONS);
 X = full(A)';
 attributeNames = cellstr(D); 
 
 %% Display the result
 display(attributeNames);
-display(X);
+
+
+%% display(X);
