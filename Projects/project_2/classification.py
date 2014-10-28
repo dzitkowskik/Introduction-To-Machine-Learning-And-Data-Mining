7b5f9c2f54ea661b49782ed539d28b3599917ef7
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-10-28 19:21:58 +0100

    + naive bayes classifier

--- Projects/project_2/classification.py
+++ Projects/project_2/classification.py
@@ -1,9 +1,18 @@
+import sys
 from pylab import *
 import numpy as np
 from scipy.io import loadmat
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
 from multiprocessing import Pool
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import RFECV
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.cross_validation import StratifiedKFold
+from sklearn import preprocessing
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 
 class Data(object):
@@ -46,26 +55,64 @@ class KNeighbors(object):
             j += 1
         errors = sum(inner_errors, 0) / float(self.internal_cross)
         print "error rates: ", errors
+        sys.stdout.flush()
         return errors.argmin() + 1
 
-    def run(self, X_train, y_train, X_test, y_test):
+    def run(self, fold, X_train, y_train, X_test, y_test):
         best_k = self.__get_best_k(X_train, y_train)
-        print "besk k: ", best_k
         knclassifier = KNeighborsClassifier(n_neighbors=best_k, p=2)
         knclassifier.fit(X_train, ravel(y_train))
         y_est = knclassifier.predict(X_test)
+        print "KNeighbors fold ", fold, " done!"
         return np.sum(ravel(y_est) != ravel(y_test)) / float(len(X_test))
 
-    def __call__(self, X_train, y_train, X_test, y_test):
-        return self.run(X_train, y_train, X_test, y_test)
+    def __call__(self, fold, X_train, y_train, X_test, y_test):
+        return self.run(fold, X_train, y_train, X_test, y_test)
+
+
+class NaiveBayes(object):
+    def __init__(self, alpha=1.0, est_prior=True, internal_cross=5):
+        self.alpha = alpha
+        self.est_prior = est_prior
+        self.internal_cross = internal_cross
+
+    def run(self, fold, X_train, y_train, X_test, y_test):
+        nb_classifier = MultinomialNB(alpha=self.alpha, fit_prior=self.est_prior)
+        le = preprocessing.LabelEncoder()
+        le.fit_transform(ravel(y_train))
+
+        feature_selector = RFECV(
+            estimator=nb_classifier,
+            step=1,
+            cv=StratifiedKFold(ravel(y_train), self.internal_cross),
+            scoring='accuracy'
+            )
+
+        classifier = Pipeline([
+            ('feature_selection', feature_selector),
+            ('classification', nb_classifier)
+            ])
+
+        classifier.fit(X_train, le.transform(ravel(y_train)))
+        result = classifier.predict(X_test)
+        y_est = le.inverse_transform(result)
+
+        print "KNeighbors fold ", fold, " done!"
+        error_sum = np.sum(ravel(y_est) != ravel(y_test))
+        return error_sum / float(len(X_test))
+
+    def __call__(self, fold, X_train, y_train, X_test, y_test):
+        return self.run(fold, X_train, y_train, X_test, y_test)
 
 
 def main():
+    np.set_printoptions(edgeitems=7)
     data = Data()
-    K = 8  # K-fold crossvalidation
-    N = 1  # number of classification algorithms
+    K = 10  # K-fold crossvalidation
+    N = 2  # number of classification algorithms
     CV = cross_validation.KFold(data.N, K)
     k_neighbours = KNeighbors()
+    naive_bayes = NaiveBayes()
     errors = np.zeros((N, K))
     pool = Pool(K*N)
     responses = {}
@@ -77,11 +124,17 @@ def main():
         y_test = data.y[test_index, :]
         responses[i, 0] = pool.apply_async(
             k_neighbours,
-            args=(X_train, y_train, X_test, y_test)
+            args=(i, X_train, y_train, X_test, y_test)
+            )
+        responses[i, 1] = pool.apply_async(
+            naive_bayes,
+            args=(i, X_train, y_train, X_test, y_test)
             )
         i += 1
     errors[0] = map((lambda x: responses[x, 0].get()), range(0, K))
-    print "k-neighbours error rates: \n", "\t\t", errors
+    errors[1] = map((lambda x: responses[x, 1].get()), range(0, K))
+    print "k-neighbours error rates: \n", "\t\t", errors[0], "\naverage: ", np.average(errors[0])
+    print "naive bayes error rates: \n", "\t\t", errors[1], "\naverage: ", np.average(errors[1])
     pool.close()
     pool.join()
     return
